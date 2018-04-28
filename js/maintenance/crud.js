2bd94ebc4e19bc118a68195af19c45e202ac588e
Jause <andirexulon@gmail.com>
2018-04-28 14:08:09 +0200

    Mejorado diseño pagina mantenimiento

--- js/maintenance/crud.js
+++ js/maintenance/crud.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
     $('#myTable').DataTable({
         "lengthChange": false,
-        "pagingType": "numbers"
+        "pagingType": "numbers",
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+        }
     });
 });
