dadfa57a2570a3dd6d76c0bed441e9e758a5c372
erasto <>
2018-05-24 20:02:04 +0200

    Ajustes en DAO Users

--- src/dao/users/UsersDAO.php
+++ src/dao/users/UsersDAO.php
@@ -13,45 +13,78 @@ use PDO;
 
 class UsersDAO {
     
-    
     public function getAllUsersDAO() {
-        $sql = "SELECT * FROM USERS";
-
-        $dbConnection = new DBConnection();
-        $db = $dbConnection->getConnection();
-        $stmt = $db->prepare($sql);
-        $stmt->execute();
-        $users = $stmt->fetchAll(PDO::FETCH_OBJ);
-        $dbConnection->disconnect();
+        
+        try{
+            
+            $dbConnection = new DBConnection();
+            $db = $dbConnection->getConnection();
+            
+            $sql = "SELECT * FROM USERS";
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
+            $users = $stmt->fetchAll(PDO::FETCH_OBJ);
+            
+        } catch (\Exception $exception) {
+            echo $exception->getMessage();
+        } finally {  
+            $dbConnection->disconnect();
+        }
 
         return $users;
     }
     
-    public function getUserDAO($user)
-    {
-        $dbConnection = new DBConnection();
+    public function getUserDAO($user){
+    
+        try{
+            $dbConnection = new DBConnection();
+            $db = $dbConnection->getConnection();
 
-        $db = $dbConnection->getConnection();
-        $stmt = $db->prepare("SELECT * FROM USERS WHERE pass=:id");
-        $stmt->bindParam(":id", $id);
-        $stmt->execute();
-        $incidencia = $stmt->fetch(PDO::FETCH_OBJ);
-        $dbConnection->disconnect();
+            $stmt = $db->prepare("SELECT * FROM USERS WHERE pass=:id");
+            $stmt->bindParam(":id", $id);
+            $stmt->execute();
+            $incidencia = $stmt->fetch(PDO::FETCH_OBJ);
+            
+        } catch (\Exception $exception) {
+            echo $exception->getMessage();
+        } finally {  
+            $dbConnection->disconnect();
+        }
+        
         return $incidencia;
     }
     
     public function addUserDAO($user)
     {
-        $dbConnection = new DBConnection();
-
-        $db = $dbConnection->getConnection();
-        $stmt = $db->prepare("INSERT INTO USERS (pass,nombre) "
-                           . "VALUES (:id,:nombre)");
-        $stmt->bindParam(":nombre", $user->nombre);
-        $stmt->bindParam(":id", $user->id);
-        $success = $stmt->execute();
-        $dbConnection->disconnect();
-        return $success;
+        try{
+            $insertado = true;
+            
+            $dbConnection = new DBConnection();
+            $db = $dbConnection->getConnection();
+            $db->beginTransaction();
+            
+            $stmt = $db->prepare("INSERT INTO USERS (pass,nombre) "
+                               . "VALUES (:id,:nombre)");
+            $stmt->bindParam(":nombre", $user->nombre);
+            $stmt->bindParam(":id", $user->id);
+            $stmt->execute();
+            $id_usuario->id_usuario = $db->lastInsertId();
+            
+            
+            $stmt2 = $db->prepare("INSERT INTO PERMISOS (id_usuario,id_permiso) "
+                               . "VALUES (:id_usuario,:id_permiso)");
+            $stmt2->bindParam(":id_usario", $id_usuario->id_usuario);
+            $stmt2->bindParam(":id_permiso", $user->id_permiso);
+            $stmt2->execute();
+            $db->commit();
+            
+        } catch (\Exception $exception) {
+            $insertado = false;
+            $db->rollBack();
+        } finally {
+            $dbConnection->disconnect();
+        }
+        return $insertado;
     }
     public function updateUserDAO($user)
     {
