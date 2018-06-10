a5a0400a720560d639009671191c169cba88817d
ricardo7227 <r.alexis.remache@gmail.com>
2018-06-10 17:47:29 +0200

    Bolsa de Trabajo - mejoras diseño y pruebas 1

--- src/config/ConfigBolsaTrabajo.php
+++ src/config/ConfigBolsaTrabajo.php
@@ -13,8 +13,6 @@ class ConfigBolsaTrabajo
 {
     //Tamaño del texto descriptivo de las ofertas mostradas en la página mostrada
     const LONGITUD_TEXTO_DESCRIPCION = 150;
-    //Número de ofertas mostradas por página
-    const NUM_RESULTADOS_OFERTAS = 10;
 
     //Directorio fotos perfiles
     const DIRECTORIO_PERFILES = "img/bolsaTrabajo/perfiles";
