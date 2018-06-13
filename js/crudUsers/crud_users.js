d60d45203f98eef78c226219a659f81d8058b877
erasto <>
2018-06-13 09:02:02 +0200

    ajustes finales

--- js/crudUsers/crud_users.js
+++ js/crudUsers/crud_users.js
@@ -1,11 +1,11 @@
-function cargarUser(id,nombre,apellidos,telefono,email,nick,nombre_rol){
+function cargarUser(id,nombre,apellidos,telefono,email,nick,id_rol){
     document.getElementById("id_user").value=id;
     document.getElementById("nombre_user").value=nombre;
     document.getElementById("apellidos_user").value=apellidos; 
     document.getElementById("telefono_user").value=telefono;
     document.getElementById("email_user").value=email;
     document.getElementById("nick_user").value=nick;
-    document.getElementById("nombre_rol").value=nombre_rol;
+    document.getElementById("id_rol").value=id_rol;
 }
         
 
